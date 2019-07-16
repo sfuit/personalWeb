7384ada901a4d778fbc4c0b7405b88c8ff3809c7
shufeng <1175620549@qq.com>
2019-07-16 10:22:23 +0800

    new project of myweb

---
+++ src/components/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="home-page">
+    <h1>Welcome To UIT's Platform</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home-page"
+};
+</script>
+
+
+<style scoped lang="scss">
+#home-page{
+  text-align: center;
+}
+</style>
---
+++ src/views/main-view.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="main-view">
+    <home-page></home-page>
+  </div>
+</template>
+
+<script>
+import HomePage from "@/components/home.vue";
+export default {
+  name: "main-view",
+  components: {
+    HomePage
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang="css" scoped>
+</style>
